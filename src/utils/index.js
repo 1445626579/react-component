4025a4d235d0a334fd35f8afa5b8a7962bcdd3cf
1445626579 <40221753+1445626579@users.noreply.github.com>
2020-08-22 14:10:03 +0800

    节流函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -20,3 +20,19 @@ export function getQueryVariable (variable) {
   }
   return false;
 }
+/**
+ *
+ * @param {function} promise 返回值为promise的函数
+ */
+export function promiseDebounce (promise) {
+  let isLoading = false
+  if (Object.prototype.toString.call(promise) !== '[object Function]') {
+    console.warn('入参必须是一个函数')
+    return () => { isLoading = false }
+  }
+  return (...rest) => {
+    if (isLoading) return
+    isLoading = true
+    promise(...rest).finally(() => { isLoading = false })
+  }
+}
